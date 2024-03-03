5c6f52bba10e9b1f3a4c3c13270006f2ac1afce8
장수영 <suyoung998@naver.com>
2024-03-03 11:08:08 +0900

    Feat : 프로젝트 추가

---
+++ DragonDance/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    enum Mode
+    {
+
+    }
+    public float speed = 5.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal"))
+        {
+
+        }
+    }
+}
