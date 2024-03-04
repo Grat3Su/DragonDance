8bdf3416b8148f1e0f46b298fa4423d38c851110
장수영 <suyoung998@naver.com>
2024-03-05 08:57:41 +0900

    Feat : ProjSetting

--- DragonDance/Assets/Scripts/Movement.cs
+++ DragonDance/Assets/Scripts/Movement.cs
@@ -4,22 +4,88 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    enum Mode
+    public enum KeyMode
     {
-
+        GETAXIS, KEYDOWN
     }
+
+    public enum MoveType
+    {
+        POSITION, TRANSLATE,RIGID
+    }
+
+    Rigidbody rigid;
+    public KeyMode kmode;
+    public MoveType mtype;
     public float speed = 5.0f;
     void Start()
     {
-        
+        rigid = gameObject.GetComponent<Rigidbody>();  
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal"))
-        {
+        float h = 0;
+        float v = 0;
 
+        // 키 타입
+        if(kmode == KeyMode.GETAXIS)
+        {
+            h = Input.GetAxis("Horizontal");
+            v = Input.GetAxis("Vertical");
+
+            h = h * speed * Time.deltaTime;
+            v = v * speed * Time.deltaTime;
         }
+        else if(kmode == KeyMode.KEYDOWN)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                v = speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.S))
+            {
+                v = -speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                h = -speed * Time.deltaTime;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                h = speed * Time.deltaTime;
+            }
+        }
+
+        // 움직임 타입
+        if(mtype == MoveType.POSITION)
+        {
+            PositionMove(h, v);
+        }
+        else if(mtype == MoveType.RIGID)
+        {
+            RigidMove(h, v);
+        }
+        else if(mtype == MoveType.TRANSLATE)
+        {
+            TranslateMove(h, v);
+        }
+    }
+
+    private void PositionMove(float h,  float v)
+    {
+        transform.position += new Vector3(h, v, 0);
+    }
+
+    void TranslateMove(float h, float v)
+    {
+        transform.Translate(Vector3.right * h);
+        transform.Translate(Vector3.up * v);
+    }
+
+    void RigidMove(float h, float v)
+    {
+        rigid.AddForce(new Vector3(h * 10, v * 10,0));
     }
 }
